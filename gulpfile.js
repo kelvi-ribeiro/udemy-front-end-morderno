c5b88b939db0581fd23cdedc58a5958943823ebd
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-09-15 02:02:31 -0300

    Aula 30. Monitorando alteracoes com o gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,9 @@ gulp.task('sass',function(){
     gulp.src('./src/sass/**/*.scss')
     .pipe(sass())
 
-    .pipe(gulp.dest('/src/css/'));
+    .pipe(gulp.dest('./src/css/'));
+})
+
+gulp.task('listen',() => {
+    gulp.watch('./src/sass/**/*.scss',['sass'])
 })
