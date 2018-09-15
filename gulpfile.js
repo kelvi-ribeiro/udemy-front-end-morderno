6feab96758d99a51ea758a068a7f073462262fad
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-09-15 01:49:31 -0300

    Aula 29. Criando a parimeira  do gulp (Compilar sass)

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-var gulp = required('gulp')
+var gulp = require('gulp'),
+sass = require('gulp-sass')
+
+gulp.task('sass',function(){
+    gulp.src('./src/sass/**/*.scss')
+    .pipe(sass())
+
+    .pipe(gulp.dest('/src/css/'));
+})
