f8c8a3161ab796880e8caf50fc6a3d99149e5689
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-09-23 16:41:06 -0300

    Aula 66. Criando os componentes necessarios e alterando a terefa da pasta do base do server

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-brwoserSync = require('browser-sync')
+var gulp = require('gulp')
+,   sass = require('gulp-sass')
+,   include = require('gulp-file-include')
+,   brwoserSync = require('browser-sync')
 
 gulp.task('sass',function(){
     gulp.src('./src/sass/**/*.scss')
     .pipe(sass())
 
     .pipe(gulp.dest('./src/css/'));
+})
+
+gulp.task('html',function(){
+    gulp.src('./src/index.html')
+    .pipe(include())
+    .pipe(gulp.dest('./dist/'))
 })
 
 gulp.task('server',function(){
@@ -17,4 +24,5 @@ gulp.task('server',function(){
     })
     gulp.watch('./src/**/**').on('change',brwoserSync.reload)
     gulp.watch('./src/sass/**/*.scss',['sass'])
+    
 })
