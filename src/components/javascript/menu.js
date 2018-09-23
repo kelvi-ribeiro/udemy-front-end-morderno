a3797a7573fab4d132c97030f91e31fc515a67f7
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-09-23 15:48:52 -0300

    Aula 63. Melhorando a usabilidade do menu  - Parte 2

--- src/components/javascript/menu.js
+++ src/components/javascript/menu.js
@@ -2,7 +2,17 @@
     'use scrict';
     var btn = document.querySelector('.header-nav__hamburgger')
     var html = document.querySelector('html');
+    var classMenu = 'menu-opened';
     var menuOpened = false;
+
+    html.addEventListener('click',function(e){
+        if(e.target === this){
+            closeMenu();
+            return;
+        }
+
+        
+    })
 
     btn.addEventListener('click',toggleMenu);
     function toggleMenu(e){
@@ -14,11 +24,12 @@
     }
     function closeMenu(){
         menuOpened = false;
-        html.classList.remove('menu-opened')
+        html.classList.remove(classMenu);
+        btn.blur();
     }
 
     function openMenu(){
         menuOpened = true;
-        html.classList.add('menu-opened')
+        html.classList.add(classMenu)
     }
 })()
