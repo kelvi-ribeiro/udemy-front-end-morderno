2e7b0d75c47d13b49bbf0fdf973a11c9874c320f
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-09-23 19:37:14 -0300

    Aula 69. Copiando apenas os arquivos necessarios e usando o gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ var gulp = require('gulp')
 ,   sass = require('gulp-sass')
 ,   include = require('gulp-file-include')
 ,   brwoserSync = require('browser-sync')
+,   clean = require('gulp-clean')
 
-gulp.task('copy',function(){
+gulp.task('clean',function(){
+    return gulp.src('dist')
+    .pipe(clean());
+})
+
+gulp.task('copy', ['clean'],function(){
     gulp.src(
             ['src/components/**/*',
             'src/css/**/*',
@@ -23,12 +29,12 @@ gulp.task('sass',function(){
 })
 
 gulp.task('html',function(){
-    gulp.src('./src/index.html')
+    return gulp.src('./src/index.html')
     .pipe(include())
     .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('server',function(){
+gulp.task('server',['html'],function(){
     brwoserSync.init({
         server:{
             baseDir:'dist'
